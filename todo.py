90cc0a8af11692cb5cdfbf29b1d0436d8527af3b
thompsongiovanni <thompson_giovanni@columbusstate.edu>
2022-09-25 14:20:01 +0000

    Finished addtodo

--- todo.py
+++ todo.py
@@ -3,34 +3,18 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-
+todoList = []
 
 @app.route("/")
 def hello():
-    return render_template('home.html')
+    return render_template('home.html', todoList=todoList)
 
 @app.route('/addTodo', methods=['POST'])
 def addTodo():
-    text1 = request.form['text1']
-    print(text1)
-    return(text1)
+    todoItem = request.form['text1']
+    todoList.append(todoItem)
+    return render_template('home.html', todoList=todoList)
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-
-
-#All 4 of them pass information through query parameters and path parameters
-# Query params. www.google.com?page=3&row=53782&user=joe
-# Path parameters www.google.com/someRootURL/3//53782/joe
-
-#POST AND PUT
-#Info will be passed in the body -> www.google.com
-
-#GET, PUT, POST, DELETE
-#GET and DELETE
-
-
-#CRUD CREATE, RETRIEVE, UPDATE, DELETE
